918ce1f8705dc55b7021b11fd8ffada36fb47310
Juan Cruz Cáceres <caceresjuan.dev@gmail.com>
2024-02-15 22:49:03 -0300

    Update NFT Marketplace Platform configuration

---
+++ app/(root)/nft/create/page.tsx
@@ -0,0 +1,3 @@
+export default async function CreateNFTPage() {
+  return <div>CreateNFT</div>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Providers } from './providers'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -11,7 +14,38 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'NFT Marketplace',
-  description: 'NFT Marketplace'
+  applicationName: 'CryptoMarket',
+  description: 'NFT Marketplace',
+  authors: {
+    name: 'Juan Cruz Cáceres',
+    url: 'https://caceresjuan.vercel.app/'
+  },
+  creator: 'Juan Cruz Cáceres',
+  publisher: 'Juan Cruz Cáceres',
+  referrer: 'no-referrer',
+  keywords: [
+    'Nextjs',
+    'Next.js 14',
+    'React',
+    'TypeScript',
+    'TailwindCSS',
+    'MongoDB',
+    'Vercel',
+    'Vercel Analytics',
+    'Vercel Speed Insights',
+    'NFT Marketplace Platform',
+    'CryptoKet',
+    'Blockchain',
+    'Ethereum',
+    'Hardhat',
+    'Solidity',
+    'Web3',
+    'Smart Contracts',
+    'IPFS',
+    'Polygon',
+    'Crypto',
+    'Juan Cruz Cáceres'
+  ]
 }
 
 export default function RootLayout({
@@ -21,7 +55,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Providers>{children}</Providers>
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
