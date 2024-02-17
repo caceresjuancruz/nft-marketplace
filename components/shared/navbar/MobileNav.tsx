88c2e17c070cabd543dc39d6e16eb2ddc4175bc3
Juan Cruz Cáceres <caceresjuan.dev@gmail.com>
2024-02-17 17:54:14 -0300

    Remove unused import and update CSS class in MobileNav component

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -8,7 +8,6 @@ import {
 } from '@/components/ui/sheet'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { sidebarLinks } from '@/constants'
 import { useTranslation } from '@/app/i18n/client'
 import { useLanguage } from '@/context/LanguageProvider'
@@ -16,23 +15,15 @@ import { useLanguage } from '@/context/LanguageProvider'
 const NavContent = () => {
   const { lng } = useLanguage()
   const { t } = useTranslation(lng, 'translations')
-  const pathname = usePathname()
-
   return (
     <section className='flex h-full flex-1 flex-col gap-6 pt-16'>
       {sidebarLinks.map(item => {
-        const isActive =
-          (pathname.includes(item.route) && item.route.length > 1) ||
-          pathname === item.route
-
         return (
           <SheetClose asChild key={item.route}>
             <Link
               href={item.route}
               aria-label={item.label}
-              className={
-                'cursor-pointer bg-transparent text-5xl text-slate-200 hover:text-white'
-              }
+              className='cursor-pointer bg-transparent text-5xl text-white'
             >
               <span className='flex items-center justify-start gap-4 p-4'>
                 <p className='font-thin'>{t(`${item.label}`)}</p>
